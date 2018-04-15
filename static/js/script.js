f3fb6c98eed2bd85c8cc4bb1c2d490146ac4cedf
vjdhanota <vjd209@gmail.com>
2018-04-14 20:38:19 -0700

    Added resizable textareas. Made lists sortable. Some refactoring.

--- static/js/script.js
+++ static/js/script.js
@@ -2,28 +2,23 @@
 // Your client side JavaScript code goes here.
 // This file is included in every page.
 
-
-
 /******************** Setup all event listeners *******************/
 //Handle the adding of a new list
 $(document).on('click','.add-list', function(){
-  createList('Hello', 1, ['Card 1'])
-  .then(() => loadLists())
-  .then((data) => displayLists(data));
-});
-
-//Handle the adding of a new list
-$(document).on('click','.delete-list', function(){
-  let id = $(this).parent('li').data('id');
-  deleteList(id)
-  .then(() => loadLists())
-  .then((data) => displayLists(data));
+  let newPosition = $("#lists").children().length + 1;
+  createList("Change me!", newPosition, ['']);
 });
 
 // Add a new card to an existing list
 $(document).on('click','.add-card', function(evt){
   let id = $(this).parent('li').data('id');
   addCardToList(id);
+});
+
+//Handle the deletion of a list
+$(document).on('click','.delete-list', function(){
+  let id = $(this).parent().parent('li').data('id');
+  deleteList(id)
 });
 
 // Delete a card from an existing list
@@ -53,6 +48,12 @@ $(document).on('click','.edit-contents', function(){
   this.setSelectionRange(0, this.value.length);
 });
 
+// Continually check for resize of textarea when typing
+$(document).on('keyup','.edit-contents', function(){
+  autoresize(this);
+});
+
+
 /***************************************************************/
 
 
@@ -65,7 +66,14 @@ function createList(name, pos, cards) {
       pos: pos,
       cards: cards
     }
-  });
+  })
+  .then(() => loadLists())
+  .then((data) => displayLists(data));
+}
+
+// Example code for getting all `list`s from server
+function loadLists() {
+  return $.ajax('/api/lists');
 }
 
 // Get a list based on its id
@@ -86,24 +94,27 @@ function updateList(id, name, pos, cards) {
       cards
     }
   })
+  .then(() => loadLists())
+  .then((data) => displayLists(data));
 }
 
 function deleteList(id) {
   return $.ajax(`/api/lists/${id}`, {
     type: 'DELETE',
   })
+  .then(() => loadLists())
+  .then((data) => displayLists(data));
 }
 
 //Delete a list with the given id
 function addCardToList(id) {
-  getList(id)
+  getList(parseInt(id))
     .then(list => {
       let newCards = list.cards !== undefined ? list.cards : []; // Handle an empty list
-      newCards.push(`Card ${newCards.length+1}`);
-      return updateList(id, list.name, list.pos, newCards);
+      newCards.push(``);
+      updateList(id, list.name, list.pos, newCards);
     })
-    .then(() => loadLists())
-    .then((data) => displayLists(data));
+    
 }
 
 //Move a card from one list to another - called by SortableJS onAdd method
@@ -113,11 +124,9 @@ function moveCardToList(newId, newIndex, oldId, oldIndex, contents) {
     .then(list => {
       let newCards = list.cards !== undefined ? list.cards : [];
       newCards.splice(newIndex,0,contents);
-      return updateList(newId, list.name, list.pos, newCards);
+      updateList(newId, list.name, list.pos, newCards);
     })
-    .then(() => loadLists())
-    .then((data) => displayLists(data));
-}
+  }
 
 // Move a card's position within a list
 function moveCardWithinList(id, oldIndex, newIndex) {
@@ -125,34 +134,30 @@ function moveCardWithinList(id, oldIndex, newIndex) {
     .then(list => {
       let newCards = list.cards !== undefined ? list.cards : [];
       arrayMove(newCards, oldIndex, newIndex);
-      return updateList(id, list.name, list.pos, newCards)
+      updateList(id, list.name, list.pos, newCards)
     })
-    .then(() => loadLists())
-    .then((data) => displayLists(data));
 }
 
 //Utility function to let us shift order of cards within a list
 function arrayMove(arr, fromIndex, toIndex) {
-  var element = arr[fromIndex];
+  let element = arr[fromIndex];
   arr.splice(fromIndex, 1);
   arr.splice(toIndex, 0, element);
 }
 
 //Delete a card from the list with the given id at the given index
 function deleteCardFromList(id, deleteIndex) {
-  getList(id)
+  getList(parseInt(id))
     .then(list => {
       list.cards.splice(parseInt(deleteIndex),1);
-      return updateList(id, list.name, list.pos, list.cards);
+      updateList(id, list.name, list.pos, list.cards);
     })
-    .then(() => loadLists())
-    .then((data) => displayLists(data));
 }
 
 
 //Update the name of a list
 function updateListName(id, newName) {
-  getList(id)
+  getList(parseInt(id))
     .then(list => {
       updateList(id, newName, list.pos, list.cards);
     })
@@ -160,19 +165,33 @@ function updateListName(id, newName) {
 
 //Update the name of a list
 function updateCardContents(id, newContents, cardPos) {
-  getList(id)
+  getList(parseInt(id))
     .then(list => {
       cardPos = parseInt(cardPos);
       list.cards[cardPos] = newContents;
       updateList(id, list.name, list.pos, list.cards);
     })
-    .then(() => loadLists())
-    .then((data) => displayLists(data));
 }
 
-// Example code for getting all `list`s from server
-function loadLists() {
-  return $.ajax('/api/lists');
+// Update the position of lists after they have been dragged around
+function updateListPositions() {
+  let currPosition = 1;
+  $("#lists").children().each(function() {
+    const id = this.dataset.id;
+    getList(parseInt(id))
+      .then(list => {
+        list.pos = currPosition;
+        updateList(id, list.name, list.pos, list.cards);
+      })
+      .then(() => currPosition++) //Need to make sure we increment currPosition only after the list has been updated
+
+  });
+}
+
+// Utility function to resize textareas
+function autoresize(textarea) {
+  textarea.style.height = '0px';     //Reset height, so that it not only grows but also shrinks
+  textarea.style.height = (textarea.scrollHeight)  + 'px';    //Set new height
 }
 
 // Example code for displaying lists in the browser
@@ -182,60 +201,77 @@ function displayLists(lists) {
   // Lists should be ordered based on their 'pos' field
   lists.rows = _.sortBy(lists.rows, 'pos');
   lists.rows.forEach(function(list) {
-    let name = $(`<input class="list-name" contenteditable="true">`).val(list.name);
+    let listHandle = $(`<span class="glyphicon glyphicon-menu-hamburger">`);
+    let name = $(`<input placeholder="Insert title here..." class="list-name" contenteditable="true">`).val(list.name);
     let deleteListButton = $(`<span class="delete-list">`).append($(`<i>`).text('✘'));    
-    let titleWrapper = $(`<div class="titleWrapper">`).append(name).append(deleteListButton);    
+    let titleWrapper = $(`<div class="title-wrapper">`).append(listHandle).append(name).append(deleteListButton);    
     let addButton = $(`<a href="#" class="add-card">`).text('Add a card...')
     let curElem = $(`<li class="list" data-id="${list.id}">`).append(titleWrapper)
+    let innerUl = $(`<ul class="card-container list-group" data-id="${list.id}">`);
     if (list.cards) {
-      let innerUl = $(`<ul class="card-container list-group" data-id="${list.id}">`);
-      //Make each list of cards draggable using the SortableJS library
-      let sortable = Sortable.create(innerUl[0], {
-        group: '.card-container',
-        handle: '.glyphicon-menu-hamburger',
-        animation: 150,
-        // Element is dropped into the list from another list
-        onAdd: function (/**Event*/evt) {
-          let cardEl = evt.item;
-          let contents = $(cardEl).find('input').val();
-          let oldListId = evt.from.dataset.id;
-          let oldIndex = evt.oldIndex;
-          let newListId = $(cardEl).parent('ul').data('id');
-          let newIndex = evt.newIndex;
-          moveCardToList(newListId, newIndex, oldListId, oldIndex, contents)
-        },
-        // Element is moved within the same list
-        onUpdate: function(evt) {
-          let listId = evt.from.dataset.id;
-          let oldIndex = evt.oldIndex;
-          let newIndex = evt.newIndex;
-          moveCardWithinList(listId, oldIndex, newIndex);
-        }
-      });
-      
       let cardIdx = 0;      
       list.cards.forEach(function(card) {
         let handle = $(`<span class="glyphicon glyphicon-menu-hamburger">`)
-        let innerContentText = $(`<input class="edit-contents">`).val(card);
+        let innerContentText = $(`<textarea rows=1 contenteditable="true" placeholder="Add something!" class="edit-contents">`).val(card);
         let deleteCardButton = $(`<span class="delete-card">`).append($(`<i>`).text('x'));
+        let cardWrapper = $(`<div class="card-wrapper">`).append(handle).append(innerContentText).append(deleteCardButton);    
         innerUl
-          .append($(`<li class="list-group-item" data-id="${list.id}" data-index="${cardIdx}">`)
+          .append($(`<li class="card list-group-item" data-id="${list.id}" data-index="${cardIdx}">`)
           .append(handle)
           .append(innerContentText)
           .append(deleteCardButton));
+
           
         cardIdx++;
       });
-      curElem.append(innerUl);
-    } else {
-      curElem.append($('<ul>'))
-    } 
-    curElem.append(addButton)
-    
-    $('#lists').append(curElem);
-  });
-}
 
+    } else {
+      // handle empty lists
+      innerUl = $(`<ul class="card-container empty-card list-group" data-id="${list.id}">`);
+    }
+    curElem.append(innerUl);    
+    curElem.append(addButton)
+
+     //Make each list of cards draggable using the SortableJS library
+     Sortable.create(innerUl[0], {
+      group: '.card-container',
+      handle: '.glyphicon-menu-hamburger',
+      animation: 150,
+      // Element is dropped into the list from another list
+      onAdd: function (/**Event*/evt) {
+        let cardEl = evt.item;
+        let contents = $(cardEl).find('input').val();
+        let oldListId = evt.from.dataset.id;
+        let oldIndex = evt.oldIndex;
+        let newListId = $(cardEl).parent('ul').data('id');
+        let newIndex = evt.newIndex;
+        moveCardToList(newListId, newIndex, oldListId, oldIndex, contents)
+      },
+      // Element is moved within the same list
+      onUpdate: function(evt) {
+        let listId = evt.from.dataset.id;
+        let oldIndex = evt.oldIndex;
+        let newIndex = evt.newIndex;
+        console.log()
+        moveCardWithinList(listId, oldIndex, newIndex);
+      }
+    });
+    // Make entire lists draggable
+    Sortable.create($('#lists')[0], {
+      group: '.list',
+      handle: '.glyphicon-menu-hamburger',
+      animation: 150,
+      onUpdate: function (/**Event*/evt) {
+        updateListPositions();
+      },
+    })
+    $('#lists').append(curElem);
+  });  
+  // Resize all textareas on rendering of the lists
+  // $("textarea").each(function () {
+  //     autoresize(this)
+  // });
+}
 
 loadLists()
   .then(function(data) {
@@ -256,5 +292,5 @@ loadLists()
           displayLists(lists);
         })
     }
-
+    
   });
